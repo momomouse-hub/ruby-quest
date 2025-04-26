8958c0291410e879f3ca7e2ff4087bfbb464fb8b
momomouse-hub <allstartsat0226@gmail.com>
2025-04-27 01:50:33 +0900

    テスト追加&Gameクラス一部修正

--- test/game_test.rb
+++ test/game_test.rb
@@ -33,10 +33,86 @@ class GameTest < Minitest::Test
 
     assert_equal :win, result[:result]
     assert_equal @game.players[0], result[:winner]
+    assert_equal 2, @game.players[0].won_card.size
 
     assert_equal "ハート", result[:cards][0].mark
     assert_equal 7, result[:cards][0].number
     assert_equal "クラブ", result[:cards][1].mark
     assert_equal 3, result[:cards][1].number
+  end
+
+  def test_game_compare_play_cards_when_player2_have_strong_card
+    @game.deck = []
+    @game.players[0].hand_cards = [Card.new("ハート", 3)]
+    @game.players[1].hand_cards = [Card.new("クラブ", 7)]
+
+    result = @game.compare_play_cards
+
+    assert_equal :win, result[:result]
+    assert_equal @game.players[1], result[:winner]
+    assert_equal 2, @game.players[1].won_card.size
+
+    assert_equal "ハート", result[:cards][0].mark
+    assert_equal 3, result[:cards][0].number
+    assert_equal "クラブ", result[:cards][1].mark
+    assert_equal 7, result[:cards][1].number
+  end
+
+  def test_game_compare_play_cards_when_draw
+    @game.deck = []
+    @game.players[0].hand_cards = [Card.new("ハート", 5)]
+    @game.players[1].hand_cards = [Card.new("クラブ", 5)]
+
+    result = @game.compare_play_cards
+    assert_equal :draw, result[:result]
+    assert_equal 0, @game.players[0].won_card.size
+    assert_equal 0, @game.players[1].won_card.size
+
+    assert_equal "ハート", result[:cards][0].mark
+    assert_equal 5, result[:cards][0].number
+    assert_equal "クラブ", result[:cards][1].mark
+    assert_equal 5, result[:cards][1].number
+  end
+
+  def test_both_players_have_hand_cards_when_players_have_cards
+    @game.players[0].hand_cards = [Card.new("ハート", 5)]
+    @game.players[1].hand_cards = [Card.new("スペード", 10)]
+
+    result = @game.both_players_have_hand_cards?
+
+    assert_equal :not_empty, result[:status]
+  end
+
+  def test_both_players_have_hand_cards_when_player_has_no_cards
+    @game.players[0].hand_cards = []
+    @game.players[1].hand_cards = [Card.new("スペード", 10)]
+
+    result = @game.both_players_have_hand_cards?
+
+    assert_equal :empty, result[:status]
+    assert_includes result[:empty_player], @game.players[0]
+  end
+
+  def test_empty_player_have_won_cards_when_player_has_no_won_cards
+    @game.players[0].hand_cards = []
+    @game.players[0].won_card = []
+    empty_players = [@game.players[0]]
+
+    result = @game.empty_player_have_won_cards?(empty_players)
+
+    assert_equal :lose, result[:status]
+    assert_equal @game.players[0], result[:loser]
+  end
+
+  def test_empty_player_have_won_cards_when_player_has_won_cards
+    @game.players[0].hand_cards = []
+    @game.players[0].won_card = [Card.new("ハート", 5)]
+    empty_players = [@game.players[0]]
+
+    result = @game.empty_player_have_won_cards?(empty_players)
+
+    assert_equal :continue, result[:status]
+    assert_equal 1, @game.players[0].hand_cards.size
+    assert_equal 0, @game.players[0].won_card.size
   end
 end
